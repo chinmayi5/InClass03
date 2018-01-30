8296d42c7b0f2fb1c682a2013e9d5868e12dcd81
Stephen Weber <stephen.weber3@gmail.com>
2018-01-29 23:39:12 -0500

    More

--- app/src/main/java/com/example/kchin/inclass03/DisplayActivity.java
+++ app/src/main/java/com/example/kchin/inclass03/DisplayActivity.java
@@ -10,12 +10,12 @@ import android.widget.TextView;
 
 public class DisplayActivity extends AppCompatActivity {
     public static final String NAME_EDIT = "name";
-    public static final String EMAIL_EDIT = "email";
-    public static final String DEPT_EDIT = "department";
-    public static final String MOOD_EDIT = "mood";
     public static final int REQ_OK = 200;
     public static final String VALUE_KEY = "value";
     private Student student;
+
+    TextView mood;
+    TextView department;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +28,8 @@ public class DisplayActivity extends AppCompatActivity {
 
         TextView name = findViewById(R.id.DisplayNamebox);
         TextView email = findViewById(R.id.DisplayEmailbox);
-        TextView department = findViewById(R.id.DisplayDeparmentbox);
-        TextView mood = findViewById(R.id.DisplayMoodbox);
+        department = findViewById(R.id.DisplayDeparmentbox);
+        mood = findViewById(R.id.DisplayMoodbox);
 
         name.setText(student.getName());
         email.setText(student.getEmail());
@@ -40,25 +40,25 @@ public class DisplayActivity extends AppCompatActivity {
 
     public void nameButtonClicked(View view){
         Intent intent = new Intent("com.example.kchin.inclass03.intent.action.VIEW");
-        intent.putExtra(NAME_EDIT, student.getName());
+        intent.putExtra(NAME_EDIT, "name");
         DisplayActivity.this.startActivityForResult(intent, REQ_OK);
     }
 
     protected void emailButtonClicked(View view){
         Intent intent = new Intent("com.example.kchin.inclass03.intent.action.VIEW");
-        intent.putExtra(EMAIL_EDIT, student.getEmail());
+        intent.putExtra(NAME_EDIT, "email");
         DisplayActivity.this.startActivityForResult(intent, REQ_OK);
     }
 
     protected void departmentButtonClicked(View view){
         Intent intent = new Intent("com.example.kchin.inclass03.intent.action.VIEW");
-        intent.putExtra(DEPT_EDIT, student.getDepartment());
+        intent.putExtra(NAME_EDIT, "department");
         DisplayActivity.this.startActivityForResult(intent, REQ_OK);
     }
 
     protected void moodButtonClicked(View view){
         Intent intent = new Intent("com.example.kchin.inclass03.intent.action.VIEW");
-        intent.putExtra(MOOD_EDIT, student.getEmail());
+        intent.putExtra(NAME_EDIT, "mood");
         DisplayActivity.this.startActivityForResult(intent, REQ_OK);
     }
 
@@ -66,9 +66,11 @@ public class DisplayActivity extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data){
         if(requestCode == REQ_OK){
             if(resultCode == RESULT_OK){
-                String value = data.getStringExtra(MOOD_EDIT);
-                Log.d("Activity3", "Setting mood!");
-                student.setMood(value);
+                String value = data.getStringExtra("mood");
+                String dept = data.getStringExtra("dept");
+                Log.d("Activity3", "Setting mood! " + value);
+                mood.setText(value);
+                department.setText(dept);
             }
         }
 
--- app/src/main/java/com/example/kchin/inclass03/EditActivity.java
+++ app/src/main/java/com/example/kchin/inclass03/EditActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 
 public class EditActivity extends AppCompatActivity {
@@ -12,37 +13,75 @@ public class EditActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit);
+        Intent intent = getIntent();
+        String type = (String) intent.getStringExtra(DisplayActivity.NAME_EDIT);
 
-        final SeekBar seek = findViewById(R.id.seekBar);
-        final String[] mood = new String[1];
-        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                mood[0] = (progress + "% Positive");
-            }
+        if(type.equals("mood")) {
+            setContentView(R.layout.activity_edit);
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
+            final SeekBar seek = findViewById(R.id.seekBar);
+            final String[] mood = new String[1];
+            seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    mood[0] = (progress + "% Positive");
+                }
 
-            }
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
+                }
 
-            }
-        });
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
 
-        Button button = findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent response = new Intent();
-                response.putExtra(DisplayActivity.MOOD_EDIT,mood[0]);
-                setResult(RESULT_OK, response);
-                finish();
-            }
-        });
+                }
+            });
+
+            Button button = findViewById(R.id.button2);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent response = new Intent();
+                    response.putExtra("mood", mood[0]);
+                    setResult(RESULT_OK, response);
+                    finish();
+                }
+            });
+        }
+
+        else if(type.equals("department")){
+            setContentView(R.layout.activity_edit_radio);
+
+            final RadioGroup departmentSelection = findViewById(R.id.radioGroup1);
+            final String[] dept = new String[1];
+
+            departmentSelection.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+                    if(checkedId == R.id.SISradioButton){
+                        dept[0] = "SIS";
+                    } else if(checkedId == R.id.CSradioButton){
+                        dept[0] = "CS";
+                    } else if(checkedId == R.id.BIOradioButton){
+                        dept[0] = "BIO";
+                    } else {
+                        dept[0] = "Other";
+                    }
+                }
+            });
+
+            Button button = findViewById(R.id.button2);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent response = new Intent();
+                    response.putExtra("dept", dept[0]);
+                    setResult(RESULT_OK, response);
+                    finish();
+                }
+            });
+        }
 
     }
 }
